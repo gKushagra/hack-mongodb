979478a31da621ecce92ae7ea9f35e5964a5011c
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-01-12 23:55:34 -0600

    updated models with user model

--- app-client/src/app/models.ts
+++ app-client/src/app/models.ts
@@ -1,3 +1,62 @@
+export interface User {
+    id: string,
+    email: string,
+    favorites: Favorite[],
+    applications: Application[]
+}
+
+export interface Favorite {
+    school: CSC_School
+}
+
+export interface Application {
+    id: string,
+    program: string,
+    deadline: Date,
+    school: CSC_School,
+    files: any,
+    notes: Note[],
+}
+
+export interface Note {
+    id: string,
+    text: string
+}
+
+/** Gov. data model - minified */
+export interface CSC_School {
+    school: {
+        zip: string,
+        city: string,
+        name: string,
+        state: string,
+        accreditor: string,
+        school_url: string,
+        accreditor_code: string,
+        price_calculator_url: string,
+    },
+    programs: CSC_Program[]
+}
+
+export interface CSC_Program {
+    cip_4_digit: string,
+    title: string,
+    school: {
+        name: string,
+        type: string,
+    },
+    credential: {
+        title: string
+    },
+}
+/** Gov. data model - minified END */
+
+export interface State {
+    STATE: string,
+    SHORT: string
+}
+
+/** DEPRECATED */
 export interface College {
     OBJECTID: number,
     NAME: string,
@@ -10,38 +69,7 @@ export interface College {
     LON: number
 }
 
-export interface State {
-    STATE: string,
-    SHORT: string
-}
-
 export interface Major {
     MAJOR: string,
     CATEGORY: string
-}
-
-export interface CSC_School {
-    "school": {
-        "zip": string,
-        "city": string,
-        "name": string,
-        "state": string,
-        "accreditor": string,
-        "school_url": string,
-        "accreditor_code": string,
-        "price_calculator_url": string,
-    },
-    "programs": CSC_Program[]
-}
-
-export interface CSC_Program {
-    "cip_4_digit": string,
-    "title": string,
-    "school": {
-        "name": string,
-        "type": string,
-    },
-    "credential": {
-        "title": string
-    },
 }
