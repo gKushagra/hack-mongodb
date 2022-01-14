9ac42842fef9f871659703c492a34bafdf5dab15
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-01-13 23:37:00 -0600

    updated router links and logout, account login

--- app-client/src/app/toolbar/toolbar.component.ts
+++ app-client/src/app/toolbar/toolbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef } from '@angular/material/dialog';
+import { AuthService } from '../auth.service';
+import { AuthDialogComponent } from '../auth/auth-dialog/auth-dialog.component';
 
 @Component({
   selector: 'app-toolbar',
@@ -7,9 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ToolbarComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public dialog: MatDialog,
+    public authService: AuthService,
+  ) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void { }
+
+  public openAuthDialog(): void {
+    const dialogRef: MatDialogRef<AuthDialogComponent> = this.dialog.open(AuthDialogComponent);
+
+    dialogRef.afterClosed().subscribe(result => {
+      console.log(`Dialog result: ${result}`);
+    });
+  }
+
+  public logout(): void {
+    localStorage.removeItem('token');
+    localStorage.removeItem('id');
+    localStorage.removeItem('email');
+    localStorage.clear(); // not req.
+    this.authService.auth.isAuthenticated = false;
+    this.authService.auth.user = null;
   }
 
 }
