42b2cc125534df7cb1d1e8da7559da2a479d9478
gKushagra <55290773+gKushagra@users.noreply.github.com>
2022-01-13 23:34:23 -0600

    endpoints for saving application, get applications, favorite

--- app-client/src/app/api.service.ts
+++ app-client/src/app/api.service.ts
@@ -12,6 +12,38 @@ export class ApiService {
 
   constructor(private httpClient: HttpClient) { }
 
+  getApplications({ id }): Promise<any> {
+    return this.httpClient.get(this.endpoint + `application/${id}`).toPromise();
+  }
+
+  saveApplication({ id, application }): Promise<any> {
+    return this.httpClient.post(this.endpoint + 'application', { id, application }).toPromise();
+  }
+
+  editApplication({ id, application }): Promise<any> {
+    return this.httpClient.put(this.endpoint + 'application', { id, application }).toPromise();
+  }
+
+  deleteApplication({ id, applicationId }): Promise<any> {
+    return this.httpClient.delete(this.endpoint + `application/${id}/${applicationId}`).toPromise();
+  }
+
+  addToFavorites({ id, favorite }): Promise<any> {
+    return this.httpClient.post(this.endpoint + 'favorite', { id, favorite }).toPromise();
+  }
+
+  getFavorites({ id }): Promise<any> {
+    return this.httpClient.get(this.endpoint + `favorite/${id}`).toPromise();
+  }
+
+  getSharedApplications({ id }): Promise<any> {
+    return this.httpClient.get(this.endpoint + `shared/${id}`).toPromise();
+  }
+
+  shareApplication({ id, application, shareTo }): Promise<any> {
+    return this.httpClient.post(this.endpoint + 'shared', { id, application, shareTo }).toPromise();
+  }
+
   getColleges(): any {
     this.httpClient.get(this.endpoint + 'colleges').subscribe(data => {
       console.log(data);
